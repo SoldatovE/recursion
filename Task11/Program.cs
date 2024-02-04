108dcd26905ca4c9f5e72bb6ae273b156c7f263c
Egor <egoos_18@mail.ru>
2024-02-04 22:33:50 +0300

    Создание папки04.02.24/22:33

---
+++ Task11/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+// Задача №1
+/*  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+ Использовать рекурсию, не использовать циклы.*/
+
+
+Console.Write("Введите натуральное  число, для подсчета в промежутке от М до N:  ");
+int integerN = Convert.ToInt32(Console.ReadLine());
+
+//int startValM = 1; // 1вариант решения
+
+Console.Write("Введите натуральное  число N, для подсчета в промежутке от М до N:  ");
+int startValМ = Convert.ToInt32(Console.ReadLine()); // 2вариант решения(не могу понять, как сделать, чтобы отрицательные цыфры не выводил,)
+
+void ShowNumbers(int startValМ, int end)
+{
+    if (startValМ == end)
+    {
+        Console.Write(startValМ);
+        return;
+    }
+
+    Console.Write(startValМ + " ");
+    ShowNumbers(startValМ + 1, end);
+
+}
+ShowNumbers(integerN, startValМ); // 2вариант решения
+//ShowNumbers(1, integerN); //1вариант решения
